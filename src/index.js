2da8358fd4240dce8089e9f15323853213518495
Hironmoy <hironmoy@gmail.com>
2021-03-01 17:24:02 +0530

    basic reset

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import './styles/main.scss';
 
-const persons = {
-  username: "Hironmoy Dhar",
-  age: 32,
-  job: "Web Developer"
-}
 
-const people = {
-  ...persons,
-  age: 23,
-  job: "Full Stack Developer"
-}
-
-console.log(persons);
-console.log(people);
