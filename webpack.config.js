f9789c66902c42248919562722cdb9d3df991f50
Hironmoy <hironmoy@gmail.com>
2021-03-10 18:17:20 +0530

    completed funfacts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 let mode = "development";
 let target = "web";
@@ -12,27 +15,54 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
       {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
         },
       },
     ],
   },
 
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 
   devtool: "source-map",
   devServer: {
+    before: function (src, server) {
+      server._watch("./src/**/*.html");
+    },
     contentBase: "./dist",
     hot: true,
-  }
-}
+  },
+};
